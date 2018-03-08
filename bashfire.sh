7f25dc4da1c73af9dee9592182b4dff9f015044f
D00MFist <leslieleopitt@gmail.com>
2018-03-07 19:38:10 -0500

    Update bashfire.sh

--- bashfire.sh
+++ bashfire.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-#while read p; do
-#	echo $p
-#done  < testfire.txt
+#Note: if have a gnmap output try the follwoing first
 
+#cat nmap.gnmap | cut -d " " -f2 | sed -e 's/\<Nmap\>//g' | sed -e 's/\<Ports\>//g' | sed '/^\s*$/d'
 
+#the above cats the file, cuts to the second file (host), removes the word Nmap, remove the word Ports, and then removes blank line
 
 while read p; do
         firefox $p
 done  < testfire.txt
 
+#note testfire is the cleaned up list of hosts you want to browse to
